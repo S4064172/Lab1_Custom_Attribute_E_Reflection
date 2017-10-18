3573bf6e1cb006d143908b1cfdfe385458fb5a88
S4064172 <32641379+S4064172@users.noreply.github.com>
2017-10-18 09:36:31 +0200

    CustomAttribute add code

--- CustomAttributeEReflection/MyLibrary/CustomLibrary.cs
+++ CustomAttributeEReflection/MyLibrary/CustomLibrary.cs
@@ -1,12 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MyAttribute;
 
 namespace MyLibrary
 {
-    public class Class1
+    public class CustomLibrary
     {
+
+        class myClass_1
+        {
+            private int _first;
+
+            myClass_1()
+            {
+                _first = 0;
+                Console.WriteLine("new value-->" + _first);
+            }
+
+            [ExecuteMe("test1","reflection","ready","relace")]
+            [ExecuteMe("test2", "reflection", "ready", "relace")]
+            public void Method1(int fisrt)
+            {
+                _first = fisrt;
+                Console.WriteLine("new value-->" + _first);
+            }
+
+            [ExecuteMe("test1", "reflection", "ready","add")]
+            [ExecuteMe("test2", "reflection", "ready", "add")]
+            public void Method2(int fisrt)
+            {
+                _first += fisrt;
+                Console.WriteLine("new value-->" + _first);
+            }
+        }
+
+
+        class myClass_2
+        {
+            private string _first;
+
+            myClass_2()
+            {
+                _first = null;
+                Console.WriteLine("new value-->" + _first);
+            }
+
+            [ExecuteMe("test1", "reflection", "ready", "relace")]
+            [ExecuteMe("test2", "reflection", "ready", "relace")]
+            public void Method1(string fisrt)
+            {
+                _first = fisrt;
+                Console.WriteLine("new value-->" + _first);
+            }
+
+            [ExecuteMe("test1", "reflection", "ready", "add")]
+            [ExecuteMe("test2", "reflection", "ready", "add")]
+            public void Method2(string fisrt)
+            {
+                _first += " "+fisrt;
+                Console.WriteLine("new value-->" + _first);
+            }
+        }
+
     }
 }
